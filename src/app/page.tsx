1e1ea056207385e18333b265e295f5b493e8bdb5
LeoU-98 <mo7amedmedo9898@gmail.com>
2025-08-11 15:49:46 +0300

    Add Filter Component UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,135 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function App() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="flex h-screen w-full items-center justify-center bg-black">
+      <div className="container mx-auto flex items-center bg-red-500">
+        <Filters />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="bg-blue-500">Question </div>
+      </div>
     </div>
   );
+}
+
+function Filters() {
+  return (
+    <form className="flex flex-col gap-5 rounded-2xl bg-gradient-to-tl from-violet-600 to-purple-800 p-5">
+      <div>
+        <label className="mb-4 block font-medium text-white" htmlFor="amount">
+          Amount:
+        </label>
+        <input
+          className="block w-full rounded-full bg-white p-3 px-5 outline-none"
+          placeholder="Enter Amount "
+          type="text"
+          id="amount"
+        />
+      </div>
+      <div>
+        <label htmlFor="default" className="mb-4 block font-medium text-white">
+          Select Category:
+        </label>
+        <select
+          id="default"
+          className="block w-full cursor-pointer rounded-xl bg-white p-2.5 text-gray-900 outline-none"
+        >
+          <option value="any">All</option>
+          <option value="9">General Knowledge</option>
+          <option value="10">Entertainment: Books</option>
+          <option value="11">Entertainment: Film</option>
+          <option value="12">Entertainment: Music</option>
+          <option value="13">Entertainment: Musicals &amp; Theatres</option>
+          <option value="14">Entertainment: Television</option>
+          <option value="15">Entertainment: Video Games</option>
+          <option value="16">Entertainment: Board Games</option>
+          <option value="17">Science &amp; Nature</option>
+          <option value="18">Science: Computers</option>
+          <option value="19">Science: Mathematics</option>
+          <option value="20">Mythology</option>
+          <option value="21">Sports</option>
+          <option value="22">Geography</option>
+          <option value="23">History</option>
+          <option value="24">Politics</option>
+          <option value="25">Art</option>
+          <option value="26">Celebrities</option>
+          <option value="27">Animals</option>
+          <option value="28">Vehicles</option>
+          <option value="29">Entertainment: Comics</option>
+          <option value="30">Science: Gadgets</option>
+          <option value="31">Entertainment: Japanese Anime &amp; Manga</option>
+          <option value="32">Entertainment: Cartoon &amp; Animations</option>
+        </select>
+      </div>
+      <div>
+        <h3 className="mb-4 font-medium text-white">Select Difficulty:</h3>
+        <ul className="overflow-hidden rounded-xl bg-white text-gray-900">
+          <li className="w-full cursor-pointer border-b border-gray-500">
+            <label className="flex w-full cursor-pointer items-center gap-2 py-3 ps-3 text-sm font-medium">
+              <input
+                type="radio"
+                value="easy"
+                name="difficulty"
+                className="h-4 w-4 cursor-pointer"
+              />
+              Easy
+            </label>
+          </li>
+          <li className="w-full cursor-pointer border-b border-gray-500">
+            <label className="flex w-full cursor-pointer items-center gap-2 py-3 ps-3 text-sm font-medium">
+              <input
+                type="radio"
+                value="medium"
+                name="difficulty"
+                className="h-4 w-4 cursor-pointer"
+              />
+              Medium
+            </label>
+          </li>
+          <li className="w-full cursor-pointer">
+            <label className="flex w-full cursor-pointer items-center gap-2 py-3 ps-3 text-sm font-medium">
+              <input
+                type="radio"
+                value="hard"
+                name="difficulty"
+                className="h-4 w-4 cursor-pointer"
+              />
+              Hard
+            </label>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="mb-4 font-medium text-gray-900 dark:text-white">
+          Select Type:
+        </h3>
+        <ul className="flex w-full items-center rounded-xl bg-white font-medium text-gray-900">
+          <li className="w-full cursor-pointer">
+            <label className="flex w-full cursor-pointer items-center gap-2 py-3 ps-3 text-sm font-medium">
+              <input
+                type="radio"
+                value="multiple"
+                name="type"
+                className="h-4 w-4 cursor-pointer"
+              />
+              Multiple Choice
+            </label>
+          </li>
+          <li className="w-full cursor-pointer border-l border-gray-500">
+            <label className="flex w-full cursor-pointer items-center gap-2 py-3 ps-3 text-sm font-medium">
+              <input
+                type="radio"
+                value="boolean"
+                name="type"
+                className="h-4 w-4 cursor-pointer"
+              />
+              True / False
+            </label>
+          </li>
+        </ul>
+      </div>
+    </form>
+  );
+}
+
+function Questions() {
+  return <div></div>;
 }
